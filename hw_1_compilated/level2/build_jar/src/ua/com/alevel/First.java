d8d6c1d79f5eccb8e301d85144f4aee1c03c6142
Sashik228 <moskalyuk30090394@gmail.com>
2023-02-11 15:10:09 +0200

    1

---
+++ hw_1_compilated/level2/build_jar/src/ua/com/alevel/First.java
@@ -0,0 +1,9 @@
+package ua.com.alevel;
+
+import org.apache.commons.lang3.StringUtils;
+public class First {
+    public static void main(String[] args) {
+        ua.com.alevel.test.Second print = new ua.com.alevel.test.Second();
+        print.printMessage("Biden");
+    }
+}
