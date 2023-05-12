97152ba78201bda0bc1c523bddd786ac8c7136ef
Sashik228 <moskalyuk30090394@gmail.com>
2023-05-13 00:55:38 +0300

    hw_10

---
+++ hw_10_mysql/src/main/java/ua/com/alevel/Main.java
@@ -0,0 +1,13 @@
+package ua.com.alevel;
+
+import ua.com.alevel.controller.Controller;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Controller controller = new Controller();
+        controller.start();
+    }
+}
